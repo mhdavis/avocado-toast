39592670c67e29d57cc4198a07d9a875aaefe4a7
michaelc.pittman <michaelc.pittman@gmail.com>
2017-10-26 10:03:30 -0400

    stying

--- client/src/App.js
+++ client/src/App.js
@@ -3,12 +3,12 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import NoMatch from "./pages/NoMatch";
 import Nav from "./components/Nav";
 import Dashboard from "./pages/Dashboard";
-<<<<<<< HEAD
+
 import SignIn from "./pages/SignIn";
 import SignUp from "./pages/SignUp";
-=======
+
 import "./App.css";
->>>>>>> master
+
 
 const App = () =>
   <Router>
